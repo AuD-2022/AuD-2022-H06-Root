c09f9d34769c0fa2e704e670c920dc6714eaede6
Oshgnacknak <osh@oshgnacknak.de>
2022-06-21 22:59:06 +0200

    Public Tests for MyDate

--- src/test/java/h06/PublicTests.java
+++ src/test/java/h06/PublicTests.java
@@ -3,6 +3,8 @@ package h06;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import java.util.Calendar;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PublicTests {
@@ -46,6 +48,47 @@ public class PublicTests {
         void testApply() {
             assertEquals(49, doubleHashing.apply(1, 6));
             assertEquals(9, doubleHashing.apply(-666, 6));
+        }
+    }
+
+    @Nested
+    class MyDateTest {
+
+        private MyDate getDate(boolean b) {
+            var cal = Calendar.getInstance();
+            cal.set(2002, Calendar.MARCH, 11, 3, 4, 5);
+            return new MyDate(cal, b);
+        }
+
+        @Test
+        void testConstructor() {
+            var date = getDate(false);
+
+            assertEquals(2002, date.getYear());
+            assertEquals(Calendar.MARCH, date.getMonth());
+            assertEquals(11, date.getDay());
+            assertEquals(3, date.getHour());
+            assertEquals(4, date.getMinute());
+            assertFalse(date.getBool());
+        }
+
+        @Test
+        void testHashWithFalse() {
+            var date = getDate(false);
+            assertEquals(200024328, date.hashCode());
+        }
+
+        @Test
+        void testHashWithTrue() {
+            var date = getDate(true);
+            assertEquals(2058635491, date.hashCode());
+        }
+
+        @Test
+        void testEquals() {
+            assertEquals(getDate(true), getDate(true));
+            assertEquals(getDate(false), getDate(false));
+            assertNotEquals(getDate(true), getDate(false));
         }
     }
 }
