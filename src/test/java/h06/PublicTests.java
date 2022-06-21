9cc93e512170668402365d309efef8f42b98879a
Oshgnacknak <osh@oshgnacknak.de>
2022-06-21 22:57:59 +0200

    Public Tests for LinearProbing

--- src/test/java/h06/PublicTests.java
+++ src/test/java/h06/PublicTests.java
@@ -7,15 +7,33 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class PublicTests {
 
+    private static final int TABLE_SIZE = 69;
+
     @Nested
     class Hash2IndexFctTest {
 
-        private final Hash2IndexFct<Integer> fct = new Hash2IndexFct<>(69, 420);
+        private final Hash2IndexFct<Integer> fct = getHash2IndexFct();
 
         @Test
         void testApply() {
             assertEquals(7, fct.apply(1));
             assertEquals(30, fct.apply(-666));
+        }
+    }
+
+    private Hash2IndexFct<Integer> getHash2IndexFct() {
+        return new Hash2IndexFct<>(TABLE_SIZE, 420);
+    }
+
+    @Nested
+    class LinearProbingTest {
+
+        private final LinearProbing<Integer> probing = new LinearProbing<>(getHash2IndexFct());
+
+        @Test
+        void testApply() {
+            assertEquals(13, probing.apply(1, 6));
+            assertEquals(36, probing.apply(-666, 6));
         }
     }
 
